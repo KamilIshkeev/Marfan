bdc723ce7839c959dad40718feac2e176ce7a837
222209 <ishkeevkamil@gmail.com>
2025-05-13 15:38:10 +0300

    ц

---
+++ WpfMarathon/Pages/MyResults.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfMarathon.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для MyResults.xaml
+    /// </summary>
+    public partial class MyResults : Page
+    {
+        public MyResults()
+        {
+            InitializeComponent();
+        }
+
+        private void grid_Results_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
